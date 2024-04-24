32278e1b34912f2e6fa3561d8c4876d8354ba9f1
Pedro Merisio <merisio.office@gmail.com>
2024-04-24 14:30:06 -0300

    Atualizando EmailSystem

--- Exercícios entregues/EmailSystem.java
+++ Exercícios entregues/EmailSystem.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
 
 public class EmailSystem{
     public static void main(String[] args){
@@ -12,30 +10,32 @@ public class EmailSystem{
         Email[] caixaPostal = new Email[10];
         
         int opcao = 0;
-        int contador = 0; //conta a quantidade de emails 
-        String usuario;
+        int contador = 0;
         
         while (opcao != 2){
             System.out.println("==== Email System ====");
             System.out.println("1 - Fazer login;");
             System.out.println("2 - Sair do programa.");
             
-            System.out.printf("Entre com uma opcao: ");
+            System.out.printf("Entre com uma opção: ");
             opcao = teclado.nextInt();
+            teclado.nextLine();
+            
+            String usuario;
             
             switch(opcao){
                 case 1:
                     System.out.printf("\nDigite o nome do usuario: ");
                     usuario = teclado.nextLine();
-                    usuario = teclado.nextLine();
+                    usuario = usuario.trim();
                     
-                    for(int i = 0; i < 10; i++){
+                    for (int i = 0; i < 10; i++){
                         if (caixaPostal[i] != null)
                             contador++;
                     }
-
+                    
                     System.out.println("Usuario "+usuario+" logado.");
-                    submenu(usuario, caixaPostal, contador);
+                    caixaPostal = submenu(usuario, caixaPostal, contador);
                     
                     break;
                 case 2:
@@ -48,24 +48,23 @@ public class EmailSystem{
         }
     }
     
-    private static void submenu(String usuario, Email[] caixaPostal, int contador){
+    private static Email[] submenu(String usuario, Email[] caixaPostal, int contador){
         Scanner teclado = new Scanner(System.in);
         
         int opcao = 0;
         int pos;
-
+        
         while(opcao != 5){
             System.out.printf("\n");
-            System.out.println("==== O que deseja fazer? ====");
+            System.out.println("==== MENU DE AÇÕES ====");
             System.out.println("1 - Escrever um email;");
             System.out.println("2 - Listar os emails;");
             System.out.println("3 - Ler um email;");
             System.out.println("4 - Excluir um email;");
-            System.out.println("5 - Arquivar caixa postal.");
-            System.out.println("6 - Restaurar caixa postal.");
-            System.out.println("7 - Fazer logout.");
+            System.out.println("5 - Restaurar backup;");
+            System.out.println("6 - Fazer logout.");
             
-            System.out.printf("Entre com uma opcao: ");
+            System.out.printf("Entre com uma opção: ");
             opcao = teclado.nextInt();
             
             switch(opcao){
@@ -77,60 +76,71 @@ public class EmailSystem{
                         break;
                     }
                     
-                    Escrever(usuario, caixaPostal);
+                    caixaPostal = Escrever(usuario, caixaPostal);
                     contador++;
-
+                    
                     System.out.printf("\n");
                     System.out.println("Sua caixa postal foi atualizada.");
-
+                    
                     break;
                 case 2:
                     System.out.printf("\n");
+                    
                     contador = 0;
-
-                    for(int i = 0; i < 10; i++){
+                    for (int i = 0; i < 10; i++){
                         if (caixaPostal[i] != null)
                             contador++;
                     }
-
+                    
                     Listar(usuario, caixaPostal, contador);
                     break;
                 case 3:
-                    System.out.printf("\nQual email voce deseja ler? ");
-                    pos = teclado.nextInt();
-                    teclado.nextLine();
-
-                    System.out.printf("\n");
-                    Ler(usuario, caixaPostal, contador, pos);
+                    if (contador != 0){
+                        System.out.printf("\nQual email voce deseja ler? ");
+                        pos = teclado.nextInt();
+                        teclado.nextLine();
+                        
+                        System.out.printf("\n");
+                        
+                        if (caixaPostal != null)
+                            Ler(usuario, caixaPostal, contador, pos);
+                        else
+                            System.out.println("Este email nao existe.");
+                    }
+                    else
+                        System.out.printf("\nSua caixa postal não possui nenhum email.\n ");
+                        
                     break;
                 case 4:
-                    System.out.printf("\nQual email voce deseja excluir? ");
-                    pos = teclado.nextInt();
-                    teclado.nextLine();
-
-                    Excluir(usuario, caixaPostal, contador, pos);
-
-                    contador = 0;
-                    for(int i = 0; i < 10; i++){
-                        if (caixaPostal[i] != null)
-                            contador++;
+                    if (contador != 0){
+                        System.out.printf("\nQual email voce deseja excluir? ");
+                        pos = teclado.nextInt();
+                        teclado.nextLine();
+                        
+                        caixaPostal = Excluir(usuario, caixaPostal, contador, pos);
+                        
+                        contador = 0;
+                        for (int i = 0; i < 10; i++){
+                            if (caixaPostal[i] != null)
+                                contador++;
+                        }
                     }
+                    else
+                        System.out.printf("\nSua caixa postal não possui nenhum email.\n ");
+                    
                     break;
                 case 5:
-                    System.out.printf("\nGerando arquivo...\n\n");
-                    gravarArquivo(caixaPostal);
-                    break;
-                case 6:
-                    caixaPostal = lerArquivo(caixaPostal, contador);
-
-                    contador = 0;
-                    for(int i = 0; i < 10; i++){
+                    System.out.printf("\nImportando arquivo...\n\n");
+                    
+                    caixaPostal = lerArquivo(caixaPostal);
+                    
+                    for (int i = 0; i < 10; i++){
                         if (caixaPostal[i] != null)
                             contador++;
                     }
-
+                    
                     break;
-                case 7:
+                case 6:
                     System.out.printf("\nSaindo...\n\n");
                     break;
                 default:
@@ -139,9 +149,10 @@ public class EmailSystem{
                     break;
             }
         }
+        return caixaPostal;
     }
     
-    private static void Escrever(String usuario, Email[] caixaPostal){
+    private static Email[] Escrever(String usuario, Email[] caixaPostal){
         Scanner teclado = new Scanner(System.in);
         
         String assunto, mensagem, destinatario;
@@ -159,93 +170,82 @@ public class EmailSystem{
                 break;
             }
         }
+        
+        return caixaPostal;
     }
     
     private static void Listar(String usuario, Email[] caixaPostal, int contador){
         int contD = 0;
         int contR = 0;
-
+        
         for (int i = 0; i < contador; i++){
-            if (caixaPostal[i] != null){
-                if (usuario.equals(caixaPostal[i].getDestinatario()))
-                    contD++;
+            if (usuario.equals(caixaPostal[i].getDestinatario()))
+                contD++;
                     
-                if (usuario.equals(caixaPostal[i].getRemetente()))
-                    contR++;
-            }
+            if (usuario.equals(caixaPostal[i].getRemetente()))
+                contR++;
         }
         
-        System.out.println("Voce possui "+contD+" email(s) na sua caixa postal e enviou "+contR+" email(s)");
+        System.out.println(usuario+" possui "+contD+" email(s) na sua caixa postal e enviou "+contR+" email(s)");
     }
     
     private static void Ler(String usuario, Email[] caixaPostal, int contador, int posicao){
-        if (posicao > contador || caixaPostal[posicao] == null)
-            System.out.println("Este email não existe.");
-
-        if (caixaPostal[posicao] != null)
-            if (usuario.equals(caixaPostal[posicao].getRemetente()) || usuario.equals(caixaPostal[posicao].getDestinatario()))
-                System.out.println(caixaPostal[posicao]);
-            else
-                System.out.println("Este email nao e destinado a voce.");
+        if (posicao + 1 > contador){
+            System.out.println("Este email nao existe.");
+        }
+        else{
+            if (caixaPostal != null)
+                if (usuario.equals(caixaPostal[posicao].getRemetente()) || usuario.equals(caixaPostal[posicao].getDestinatario()))
+                    System.out.println(caixaPostal[posicao]);
+                else
+                     System.out.println("Este email nao e destinado a voce.");
+        }
     }
-
-    private static void Excluir(String usuario, Email[] caixaPostal, int contador, int posicao){
+    
+    private static Email[] Excluir(String usuario, Email[] caixaPostal, int contador, int posicao){
         if (caixaPostal[posicao] != null){
             if (usuario.equals(caixaPostal[posicao].getRemetente())){
                 if (posicao >= 0 && posicao < contador){
-                    for (int i = posicao; i < contador - 1; i++)
+                    for (int i = posicao; i < contador - 1; i++){
                         caixaPostal[i] = caixaPostal[i + 1];
-                    
+                    }
                     caixaPostal[contador - 1] = null;
-
+                    
                     System.out.printf("\n");
                     System.out.println("Email excluido com sucesso.");
-                } 
+                    
+                    return caixaPostal;
+                }
             }
             else{
                 System.out.printf("\n");
-                System.out.println("Voce nao tem autorizacao para excluir este email.");
+                System.out.println("Voce nao tem autorizacao para excluir esse email.");
+                
+                return caixaPostal;
             }
         }
         else{
             System.out.printf("\n");
             System.out.println("Este email nao existe.");
+            
+            return caixaPostal;
         }
+        return null;
     }
-
-    private static void gravarArquivo(Email[] caixaPostal){
-        File arquivo = new File("backup.dat");
-
-        try{
-            FileOutputStream fout = new FileOutputStream(arquivo);
-            ObjectOutputStream oos = new ObjectOutputStream(fout);
-
-            oos.writeObject(caixaPostal);
-            oos.flush();
-            oos.close();
-            fout.close();
-        }
-        catch (Exception ex){
-            System.out.println("erro: "+ ex.toString());
-        }
-    }
-
-    private static Email[] lerArquivo(Email[] caixaPostal, int contador){
+    
+    private static Email[] lerArquivo(Email[] caixaPostal){
         File arquivo = new File("backup.dat");
         
         try{
             FileInputStream fin = new FileInputStream(arquivo);
             ObjectInputStream oin = new ObjectInputStream(fin);
-
+            
             caixaPostal = (Email[]) oin.readObject();
             oin.close();
             fin.close();
-
-            contador = 0;
-
+            
             for (Email p : caixaPostal){
                 if (p != null){
-                    contador++;
                     System.out.println(p);
                 }
             }
@@ -253,8 +253,9 @@ public class EmailSystem{
         catch (Exception ex){
             System.err.println("erro: "+ex.toString());
         }
-        System.out.printf("\nArquivos importados com sucesso!\n\n");
-
+        System.out.println("\n\nArquivos importados com sucesso!");
+        
         return caixaPostal;
+    
     }
 }
